7ca443ec83fb24d270fef012b6d52e4a34010789
Hawty1 <49696027+mariusErpenbach@users.noreply.github.com>
2022-11-15 06:12:26 +0100

    dev setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Introduction from './components/Introduction';
+
+function App() {
+  return (
+    <div className="App">
+     <Introduction></Introduction>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Introduction.tsx
@@ -0,0 +1,21 @@
+const Introduction = () => {
+return (
+    <div id="introduction">
+        <p>
+        <h1>Your Body Is Your Temple</h1>
+        <br></br>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+        At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        </p>
+        <img id="examplePicture" src="/img/man-g54d843fa1_1920.jpg"></img>
+    </div>
+
+)
+
+}
+
+export default Introduction;
